9dc5119b7c03ebce1c0f5977d7d0bd2b81ca6d2f
masxkip <jesufemiketiku@gmail.com>
2025-08-25 12:21:04 -0700

    New commit

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -5,23 +5,12 @@ const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    console.log("Scrolling to top for", pathname);
-
-    // Smooth scroll to top of the window
+    // Smooth scroll to the top of the window
     window.scrollTo({
       top: 0,
       left: 0,
       behavior: "smooth",
     });
-
-    // If you also want to handle a scrollable container
-    const container = document.querySelector(".main-content");
-    if (container) {
-      container.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-    }
   }, [pathname]);
 
   return null;
