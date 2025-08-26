19e3380e42bf4c5559f44b4827d38fc15e06c5a8
masxkip <jesufemiketiku@gmail.com>
2025-08-26 04:40:07 -0700

    New commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,35 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import React from "react";
-import SiteLayout from "./layouts/SiteLayout";
-import Home from "./pages/Home";
-import Quote from "./pages/Quote";
-import Contact from "./pages/Contact";
-import Service from "./pages/Service";
-import About from "./pages/About";
-import Fqs from "./pages/Fqs";
-import ScrollManager from "./components/ScrollManager"; // ✅
-
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Router>
-      <ScrollManager />
-      <Routes>
-        <Route element={<SiteLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/quote" element={<Quote />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/services" element={<Service />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/faq" element={<Fqs />} />
-        </Route>
-      </Routes>
-    </Router>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
